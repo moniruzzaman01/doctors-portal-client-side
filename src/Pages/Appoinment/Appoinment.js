026d5c7737b086f2c4f7afc55ed5a7f71d1af9e5
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2022-05-11 03:56:40 +0600

    available appoinment section data make dynamic

--- src/Pages/Appoinment/Appoinment.js
+++ src/Pages/Appoinment/Appoinment.js
@@ -1,46 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import chair from "../../assets/images/chair.png";
 import Footer from "../Shared/Footer";
 import AvailableAppointment from "./AvailableAppointment";
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
+import { format } from "date-fns";
+import axios from "axios";
+import appoinmentBg from "../../assets/images/bg.png";
 
 const Appoinment = () => {
-  const recentAppontment = [
-    {
-      name: "Teeth Orthodontics",
-      time: "8:00 am - 9:00 am",
-      spaces: "10 spaces available",
-    },
-    {
-      name: "Cosmetic Dentistry",
-      time: "8:00 am - 9:00 am",
-      spaces: "10 spaces available",
-    },
-    {
-      name: "Teeth Cleaning",
-      time: "8:00 am - 9:00 am",
-      spaces: "10 spaces available",
-    },
-    {
-      name: "Teeth Orthodontics",
-      time: "8:00 am - 9:00 am",
-      spaces: "10 spaces available",
-    },
-    {
-      name: "Teeth Orthodontics",
-      time: "8:00 am - 9:00 am",
-      spaces: "10 spaces available",
-    },
-    {
-      name: "Teeth Orthodontics",
-      time: "8:00 am - 9:00 am",
-      spaces: "10 spaces available",
-    },
-  ];
+  const [date, setDate] = useState(new Date());
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    axios("Service.json").then((res) => setServices(res.data));
+  }, []);
+
+  console.log(services);
   return (
     <section>
-      <div className="hero lg:my-20">
+      <div
+        style={{
+          background: `url(${appoinmentBg})`,
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+        }}
+        className="hero lg:py-20"
+      >
         <div className="hero-content flex-col lg:flex-row-reverse">
           <img
             src={chair}
@@ -48,23 +34,29 @@ const Appoinment = () => {
             alt=""
           />
           <div className=" lg:mr-10">
-            <DayPicker />
+            <DayPicker mode="single" selected={date} onSelect={setDate} />
           </div>
         </div>
       </div>
       <div className="my-10">
-        <h4 className="text-primary text-center text-3xl mb-10 lg:mb-20">
-          Available Appointments on April 30,2022
+        <h4 className="text-primary text-center text-3xl mb-10 lg:my-20">
+          Available Appointments on {format(date, "PP")}
         </h4>
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 lg:mx-20 mx-5">
-          {recentAppontment.map((reApp, key) => (
+          {services.map((service, key) => (
+            <AvailableAppointment
+              key={key}
+              service={service}
+            ></AvailableAppointment>
+          ))}
+          {/* {recentAppontment.map((reApp, key) => (
             <AvailableAppointment
               key={key}
               name={reApp.name}
               time={reApp.time}
               spaces={reApp.spaces}
             ></AvailableAppointment>
-          ))}
+          ))} */}
         </div>
       </div>
       <Footer />
--- src/Pages/Appoinment/AvailableAppointment.js
+++ src/Pages/Appoinment/AvailableAppointment.js
@@ -1,13 +1,18 @@
 import React from "react";
 import Modal from "../Shared/Modal";
 
-const AvailableAppointment = ({ name, time, spaces }) => {
+const AvailableAppointment = ({ service }) => {
+  const { name, slots } = service;
   return (
     <div className="card lg:max-w-lg bg-slate-100 shadow-xl">
       <div className="card-body">
         <h2 className="card-title mx-auto text-primary font-bold">{name}</h2>
-        <p className=" text-center uppercase">{time}</p>
-        <p className=" text-center uppercase">{spaces}</p>
+        <p className=" text-center uppercase">
+          {slots.length ? slots[0] : "---------------"}
+        </p>
+        <p className=" text-center uppercase">
+          {slots.length} {slots.length > 0 ? "slots" : "slot"} Available
+        </p>
         <div className="flex justify-center">
           <Modal>
             <label
